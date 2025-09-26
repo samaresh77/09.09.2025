588e25045feb6d3e7cc1cbd8158754efb876cad3
Samaresh <samareshmondal53547@gmail.com>
2025-09-26 09:04:15 +0530

    updated

--- Javascript/Practice/a.js
+++ Javascript/Practice/a.js
@@ -1,2 +1,29 @@
-console.log("Hello Samaresh");
-console.log("Hello World");
+// switch case
+
+let day = 1;
+
+switch(day) {
+    case 1:
+        console.log("Monday");
+        // break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    case 7:
+        console.log("Sunday");
+        break;
+    default:
+        console.log("Invalid day");
+}
