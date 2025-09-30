a0bcb4ff192054b0fb465191506be30a02833bb7
Samaresh <samareshmondal53547@gmail.com>
2025-09-30 10:13:04 +0530

    hol

---
+++ Javascript/Tut/08/HOLoops.js
@@ -0,0 +1,26 @@
+// for of
+
+const arr = [10, 20, 30, 40, 50];
+
+// for of loop  
+for (const value of arr) {
+    console.log("Value:", value);
+}
+
+//string iteration using for of
+const str = "Hello";
+for (const char of str) {
+    console.log("Character:", char);
+}
+
+//object iteration using for of with Object.keys()
+const obj = {a: 1, b: 2, c: 3};
+for (const key of Object.keys(obj)) {
+    console.log("Key:", key, "Value:", obj[key]);
+}
+
+// for in loop (not recommended for arrays, but works)
+for (const index in arr) {
+    console.log("Index:", index, "Value:", arr[index]);
+}
+
