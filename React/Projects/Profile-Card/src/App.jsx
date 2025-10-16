15e621cc9aad81019c48a02778726502d58266a9
Samaresh <samareshmondal53547@gmail.com>
2025-10-16 21:56:38 +0530

    card

---
+++ React/Projects/Profile-Card/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Card from './component/Card'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Card />
+    </>
+  )
+}
+
+export default App
