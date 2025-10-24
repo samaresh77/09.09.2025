0f21ed32d3963861af932a6a544582d6b52c87c2
Samaresh <samareshmondal53547@gmail.com>
2025-10-24 22:18:57 +0530

    added

--- Javascript/Projects/Number-Pattern/script.js
+++ Javascript/Projects/Number-Pattern/script.js
@@ -1,45 +1,22 @@
-let btn1 = document.getElementById("1");
-let btn2 = document.getElementById("2");
-let btn3 = document.getElementById("3");
-let btn4 = document.getElementById("4");
-let btn5 = document.getElementById("5");
-let btn6 = document.getElementById("6");
-let btn7 = document.getElementById("7");
-let btn8 = document.getElementById("8");
-let btn9 = document.getElementById("9");
-let btn0 = document.getElementById("0");
-
-
-
+let buttons = document.querySelectorAll("button");
 let num1 = document.getElementById("num1");
 let num2 = document.getElementById("num2");
 let num3 = document.getElementById("num3");
 let num4 = document.getElementById("num4");
 
-btn1.onclick = function() {
-    num1.innerHTML = "1";
-}
-btn2.onclick = function() {
-    num1.innerHTML = "2";
-}
-btn3.onclick = function() {
-    num1.innerHTML = "3";
-}   
-btn4.onclick = function() {
-    num1.innerHTML = "4";
-}
-btn5.onclick = function() {
-    num1.innerHTML = "5";
-}
-btn6.onclick = function() {
-    num1.innerHTML = "6";
-}
-btn7.onclick = function() {
-    num1.innerHTML = "7";
-}
-btn8.onclick = function() {
-    num1.innerHTML = "8";
-}
-btn9.onclick = function() {
-    num1.innerHTML = "9";
-}
+buttons.forEach((btn) => {
+    // console.log(btn.textContent);
+    btn.addEventListener("click", () => {
+        let number = btn.textContent;
+        switch (number) {
+            case "1":
+                num1.textContent = number;
+                break;
+            case "2":
+                num2.textContent = number;
+                break;
+            default:
+                break;
+        }
+    });
+});
